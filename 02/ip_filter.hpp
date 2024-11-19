91babc42b9734478ca51b51074152b2a807212ba
Arina <akorosteleva04@mail.ru>
2024-11-19 21:27:00 +0700

    laba daun

---
+++ 02/ip_filter.hpp
@@ -0,0 +1,215 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+
+/**
+ * @brief Функция для лексикографической сортировки от меньшего к большему.
+ * @param a кусок первого айпи
+ */
+auto LexiSort = [](auto a, auto b)
+{
+    if (a == b) return false;
+    for (auto i = 0; i < 4; i++)
+    {   
+        if (a[i] > b[i])
+        {
+            return false;
+        }
+        else if (a[i] < b[i])
+        {
+            return true;
+        }
+    }
+    return false;
+};
+
+auto PrintAll = [](std::vector<std::vector<int>> ip_address)
+{
+    for (size_t i = 0; i < ip_address.size(); i++)
+    {
+        for (auto j = 0; j < 4; j++)
+        {
+            std::cout << ip_address[i][j];
+            if (j < 3) std::cout << ".";
+        } 
+        std::cout << std::endl;
+    }
+};
+
+auto PrintByteOne = [](std::vector<std::vector<int>> ip_address)
+{
+    for (size_t i = 0; i < ip_address.size(); i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            if (ip_address[i][0] == 1)
+            {
+                std::cout << ip_address[i][j];
+                if (j < 3) std::cout << ".";
+                else if (j == 3) std::cout << std::endl;
+                else break;
+            }
+
+        } 
+    }
+};
+
+auto Print46and70 = [](std::vector<std::vector<int>> ip_address)
+{
+    for (size_t i = 0; i < ip_address.size(); i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            if (ip_address[i][0] == 46 && ip_address[i][1] == 70)
+            {
+                std::cout << ip_address[i][j];
+                if (j < 3) std::cout << ".";
+                else if (j == 3) std::cout << std::endl;
+                else break;
+            }
+        } 
+    }
+};
+
+auto Print46 = [](std::vector<std::vector<int>> ip_address)
+{
+    for (size_t i = 0; i < ip_address.size(); i++)
+    {
+        for (size_t j = 0; j < 4; j++)
+        {
+            if ( std::find(ip_address[i].begin(), ip_address[i].end(), 46) != ip_address[i].end() )
+            {
+                std::cout << ip_address[i][j];
+                if (j < 3) std::cout << ".";
+                else if (j == 3) std::cout << std::endl;
+                else break;
+            }
+        } 
+    }
+};
+
+std::vector<std::string> split(const std::string &str, char d)
+{
+    std::vector<std::string> r;
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.push_back(str.substr(start, stop - start));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r.push_back(str.substr(start));
+    return r;
+}
+// #pragma once
+// #include <cassert>
+// #include <cstdlib>
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// // ("",  '.') -> [""]
+// // ("11", '.') -> ["11"]
+// // ("..", '.') -> ["", "", ""]
+// // ("11.", '.') -> ["11", ""]
+// // (".11", '.') -> ["", "11"]
+// // ("11.22", '.') -> ["11", "22"]
+
+// /** 
+//  *   разделение строки по символу d(".")
+//  * @param str строка на входе 
+//  * @param d символ, по которому разделяется строка
+//  */
+// std::vector<std::string> split(const std::string &str, char d)
+// {
+//     std::vector<std::string> r;
+
+//     std::string::size_type start = 0;
+//     std::string::size_type stop = str.find_first_of(d);
+//     while(stop != std::string::npos)
+//     {
+//         r.push_back(str.substr(start, stop - start));
+
+//         start = stop + 1;
+//         stop = str.find_first_of(d, start);
+//     }
+
+//     r.push_back(str.substr(start));
+
+//     return r;
+// }
+
+
+        // Считать построчно адреса запаковать???
+
+        // 222.173.235.246
+        // 222.130.177.64
+        // 222.82.198.61
+        // ...
+        // 1.70.44.170
+        // 1.29.168.152
+        // 1.1.234.8
+
+        // TODO filter by first byte and output
+        // ip = filter(1)
+
+        // 1.231.69.33
+        // 1.87.203.225
+        // 1.70.44.170
+        // 1.29.168.152
+        // 1.1.234.8
+
+        // TODO filter by first and second bytes and output
+        // ip = filter(46, 70)
+
+        // 46.70.225.39
+        // 46.70.147.26
+        // 46.70.113.73
+        // 46.70.29.76
+
+        // TODO filter by any byte and output
+        // ip = filter_any(46)
+
+        // 186.204.34.46
+        // 186.46.222.194
+        // 185.46.87.231
+        // 185.46.86.132
+        // 185.46.86.131
+        // 185.46.86.131
+        // 185.46.86.22
+        // 185.46.85.204
+        // 185.46.85.78
+        // 68.46.218.208
+        // 46.251.197.23
+        // 46.223.254.56
+        // 46.223.254.56
+        // 46.182.19.219
+        // 46.161.63.66
+        // 46.161.61.51
+        // 46.161.60.92
+        // 46.161.60.35
+        // 46.161.58.202
+        // 46.161.56.241
+        // 46.161.56.203
+        // 46.161.56.174
+        // 46.161.56.106
+        // 46.161.56.106
+        // 46.101.163.119
+        // 46.101.127.145
+        // 46.70.225.39
+        // 46.70.147.26
+        // 46.70.113.73
+        // 46.70.29.76
+        // 46.55.46.98
+        // 46.49.43.85
+        // 39.46.86.85
+        // 5.189.203.46
+  
--- main.cpp
+++ main.cpp
@@ -1,5 +1,66 @@
-#include <iostream>
+// #include <iostream>
+// #include <vector>
+// #include <string>
+// #include "02/ip_filter.hpp"
+// #include <fstream>
 
-int main(int, char**){
-    std::cout << "Hello, from lab2!\n";
+// int main()
+// {
+    
+//     std::vector<std::vector<std::string> > ip_pool;
+//     std::ifstream file("../02/ip_filter.tsv");
+//     for(std::string line; std::getline(file, line);)
+//     {
+//         std::vector<std::string> v = split(line, '\t');
+//         ip_pool.push_back(split(v.at(0), '.'));
+//     }
+
+//     /**
+//      * @brief TODO reverse lexicographically sort
+//      */
+//     for(std::vector<std::vector<std::string> >::const_iterator ip = ip_pool.cbegin(); ip != ip_pool.cend(); ++ip)
+//     {
+//         for(std::vector<std::string>::const_iterator ip_part = ip->cbegin(); ip_part != ip->cend(); ++ip_part)
+//         {
+//             if (ip_part != ip->cbegin())
+//             {
+//                 std::cout << ".";
+
+//             }
+//             std::cout << *ip_part;
+//         }
+//         std::cout << std::endl;
+//     }
+    
+// }
+
+
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include "02/ip_filter.hpp"
+
+
+int main()
+{
+    std::vector<std::vector<int>> ip_address;
+    std::string s;
+    std::ifstream file("../02/ip_filter.tsv");
+    while(std::getline(file, s))
+    {
+        auto splited = split(s.substr(0, s.find('\t')), '.');
+        std::vector<int> octets = {stoi(splited[0]), stoi(splited[1]), stoi(splited[2]), stoi(splited[3])};
+        ip_address.push_back(octets);
+    }
+
+    sort(ip_address.begin(), ip_address.end(), LexiSort);
+    PrintAll(ip_address);
+    PrintByteOne(ip_address);
+    Print46and70(ip_address);
+    Print46(ip_address);
 }
