30e215229c4578f424c3d16a33b494b5a2911e03
Arina <akorosteleva04@mail.ru>
2024-11-20 15:31:21 +0700

    done&

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Арина/labw2
+# Install script for directory: C:/Programming/labw2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/Арина/labw2/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Programming/labw2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
